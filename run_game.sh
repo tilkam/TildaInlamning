ae2189d119380c2c6661dc3c5dd2a7fd747f454f
Tilda Kampman <tilda.kampman@gmail.com>
2022-11-10 22:04:42 +0100

    script changes dir and prints location

--- run_game.sh
+++ run_game.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 NAME="Tilda_Kampman"
+echo $NAME program
 mkdir Tilda_Kampman_labb
 cp Guesser.java Tilda_Kampman_labb
 cp GuessingGame.java Tilda_Kampman_labb
-echo "$NAME"
+cd Tilda_Kampman_labb
+echo Running game from:
+pwd
 
 
