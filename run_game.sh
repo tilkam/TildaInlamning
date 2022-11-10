65e023373528902f2e82a1e87566d91a261f327c
Tilda Kampman <tilda.kampman@gmail.com>
2022-11-10 22:11:08 +0100

    script compiles and runs java files, remove class and print ls

--- run_game.sh
+++ run_game.sh
@@ -7,5 +7,14 @@ cp GuessingGame.java Tilda_Kampman_labb
 cd Tilda_Kampman_labb
 echo Running game from:
 pwd
+echo Compiling game
+javac GuessingGame.java
+echo Running Game
+java GuessingGame
+echo Done!
+rm Guesser.class
+rm GuessingGame.class
+echo Removing class files
+ls
 
 
